a8678fff434aba8c1dfcb388047d0cb6c48e0265
Obed Zavaleta <obedzavaleta17@gmail.com>
2023-01-02 11:24:55 -0600

    formulario login

--- funciones.js
+++
@@ -1,9 +0,0 @@
-function print_message(event){
-    event.preventDefault();
-    var nombre = document.getElementById("nombre").value
-    console.log(nombre)
-}
-
-function borrar(){
-    document.getElementById("nombre").value = ''
-}
